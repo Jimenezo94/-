254e41cf4a2864c0e0d1e2229889d94ec8d35b2f
Juliana Jimenez <nani.jimenez@hotmail.com>
2022-02-04 16:01:34 -0500

    practicastyled

---
+++ src/components/Component2.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {StyledContainer2} from '../styled/StyledComponents2';
+import {StyledH2} from '../styled/StyledComponents2';
+
+export default class Component2 extends Component {
+  render() {
+    return <StyledContainer2>
+        <StyledH2>Soy el componente 2</StyledH2>
+    </StyledContainer2>;
+  }
+}
